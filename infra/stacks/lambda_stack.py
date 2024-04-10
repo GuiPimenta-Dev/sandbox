5fa017f5fb410d945a9052d87eed4d934587960d
Guilherme Alves Pimenta <guialvespimenta27@gmail.com>
2024-04-09 21:00:20 -0300

    image to qr code

---
+++ infra/stacks/lambda_stack.py
@@ -0,0 +1,13 @@
+from aws_cdk import Stack
+from constructs import Construct
+from infra.services import Services
+from lambda_forge import release
+
+
+@release
+class LambdaStack(Stack):
+    def __init__(self, scope: Construct, context, **kwargs) -> None:
+
+        super().__init__(scope, f"{context.name}-Lambda-Stack", **kwargs)
+
+        self.services = Services(self, context)
