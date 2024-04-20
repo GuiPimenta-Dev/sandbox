ecaf79841c36287c7339683f6453ae16ef056000
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-04-20 12:41:35 -0300

    steps locally

--- infra/steps/steps.py
+++ infra/steps/steps.py
@@ -1,27 +1,23 @@
 from aws_cdk import pipelines as pipelines
-from aws_cdk import aws_codebuild as codebuild
-from infra.steps import create_step
+from infra.steps.codebuild import CodeBuild
 
 
 class Steps:
     def __init__(self, scope, context, source):
-        self.scope = scope
-        self.context = context
-        self.source = source
+        self.codebuild = CodeBuild(scope, context, source)
 
     def run_unit_tests(self):
 
-        report_group = {
-            "scope": self.scope,
-            "name": f"{self.context.stage}-{self.context.name}-UnitTestsReport",
-            "files": "test-results.xml",
-            "base-directory": "pytest-report",
-            "file-format": "JUNITXML",
-        }
+        partial_build_spec, permissions = self.codebuild.create_report_group(
+            name="UnitTestsReport",
+            files="test-results.xml",
+            base_directory="pytest-report",
+            file_format="JUNITXML",
+        )
 
-        return create_step(
-            source=self.source,
-            name=f"{self.context.stage}-{self.context.name}-UnitTests",
+        return self.codebuild.create_step(
+            name="UnitTests",
             commands=['pytest --junitxml=pytest-report/test-results.xml -k "unit.py"'],
-            report_group=report_group,
+            partial_build_spec=partial_build_spec,
+            permissions=permissions,
         )
