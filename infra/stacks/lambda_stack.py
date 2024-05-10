7b904d193f8195221c730f6723842c920bbdc58e
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-05-10 18:09:39 -0300

    improving folders

--- infra/stacks/lambda_stack.py
+++ infra/stacks/lambda_stack.py
@@ -1,17 +1,18 @@
-from functions.hello_world.config import HelloWorldConfig
+from docs.config import DocsConfig
 from aws_cdk import Stack
 from constructs import Construct
 from infra.services import Services
-from lambda_forge import release
+
+from lambda_forge.trackers import reset
 
 
-@release
+@reset
 class LambdaStack(Stack):
     def __init__(self, scope: Construct, context, **kwargs) -> None:
 
-        super().__init__(scope, f"{context.stage}-{context.name}-Lambda-Stack", **kwargs)
+        super().__init__(scope, f"{context.name}-Lambda-Stack", **kwargs)
 
         self.services = Services(self, context)
 
-        # HelloWorld
-        HelloWorldConfig(self.services)
+        # Docs
+        DocsConfig(self.services)
